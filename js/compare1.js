e65a242dfce9258f055be2012cd76399c667d5e6
Andrea Židkova <zidkovaandrea66@gmail.com>
2020-11-16 20:34:21 +0100

    compare 04

--- js/compare1.js
+++ js/compare1.js
@@ -12,7 +12,20 @@ let person2 = {
 
 }
 
+function percent() {
+    if (person1.height < person2.height) {
+        document.getElementById('person1').style.width = (person1.height * 100) / person2.height + '%';
+    } else if (person1.height > person2.height) {
+        document.getElementById('person2').style.width = (person2.height * 100) / person1.height + '%';
+    } else if (person1.height == person2.height) {
+        document.getElementById('person1').style.width = '100%';
+    document.getElementById('person2').style.width = '100%';
+    }
+}
+
 document.getElementById('btn').addEventListener('click', function () {
+    document.getElementById('person1').style.width = '100%';
+    document.getElementById('person2').style.width = '100%';
     person1.firstname = document.getElementById('firstname1').value;
     person2.firstname = document.getElementById('firstname2').value;
     person1.height = document.getElementById('height1').value;
@@ -23,15 +36,16 @@ document.getElementById('btn').addEventListener('click', function () {
     console.log(person2)
     document.getElementById('nametag1').innerText = person1.firstname;
     document.getElementById('nametag2').innerText = person2.firstname;
-    if (person1.sex == "m"){
+    if (person1.sex == "m") {
         document.getElementById('person1').src = 'img/man.png';
     } else {
         document.getElementById('person1').src = 'img/woman.png';
     }
 
-    if (person2.sex == "m"){
+    if (person2.sex == "m") {
         document.getElementById('person2').src = 'img/man.png';
     } else {
         document.getElementById('person2').src = 'img/woman.png';
     }
+    percent()
 })
